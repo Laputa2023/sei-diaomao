01889bbfdb452bf10d5a15ff004b90e57660c078
Laputa2023 <88488582+Laputa2023@users.noreply.github.com>
2023-12-20 03:14:00 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen items-center justify-center space-x-48 p-24">
       <div className="flex flex-col">
-        <h1 className="text-4xl font-bold">主打一个安全</h1>
+        <h1 className="text-4xl font-bold">叼毛 叼毛 叼毛</h1>
         <p className="text-xs mt-2 text-gray-400"></p>
         <Image src="/shareQrcode.jpeg" width={360} alt="" height={400} />
         <div className="flex justify-center mb-2">
